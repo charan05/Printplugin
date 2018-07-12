79ad2bd9897a72745919bfa2bd4e61d77c419b5e
charan05 <ch12jan1998@gmail.com>
2018-07-12 18:18:32 +0530

    first commit

---
+++ HelloWorld.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:     Hello World
+ * Description:     Print Welcome Message
+ * Author:          CSK
+ * Text Domain:     remove-medium-cross-links
+ * Version:         1.0
+ */
+echo "Hello World";
+?>
