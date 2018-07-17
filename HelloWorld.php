be0e0ebe1306ec83b75674603d0877c9f3dd5d4a
charan05 <ch12jan1998@gmail.com>
2018-07-17 18:10:24 +0530

    Fixed so and so bugs

--- HelloWorld.php
+++ HelloWorld.php
@@ -3,8 +3,9 @@
  * Plugin Name:     Hello World
  * Description:     Print Welcome Message
  * Author:          CSK
- * Text Domain:     remove-medium-cross-links
- * Version:         1.0
+ * Version:         2.1
  */
 echo "Hello World";
+echo "This is my update!!";
+echo "Enjoy!!";
 ?>
